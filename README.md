e29d1fb5d350f1bd763a32b7555404a65a1bb11f
Joe Siebert <joe@bluehousegroup.com>
2018-06-28 16:18:13 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Unique Page Module for SilverStripe 4
+
+## Description
+The Unique Page module prevents admin users from creating more than one instance of the page class that it's applied to.
+
+## Config
+```
+ExamplePage:
+  extensions:
+    - BluehouseGroup\UniquePage\Extensions\UniquePage
+```
---
+++ src/extensions/UniquePage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BluehouseGroup\UniquePage\Extensions
+
+use SilverStripe\Core\Extension;
+
+class UniquePage extends Extension
+{
+    public function canCreate($member = null, $context = array())
+    {
+        $class_name = $this->owner->ClassName;
+        return $class_name::get()->count() === 0;
+    }
+}
